00b9fffae0443a61d200513d8788edff9a2855f4
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-11-29 20:14:56 +0500

    updated

---
+++ models/ServicesModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const {Schema}=mongoose
+
+const serviceSchema = new Schema({
+    title: {
+        type: String,
+        default:""
+    },
+    content: {
+        type: String,
+        default:""
+    },
+    service_image:{
+        type: String,
+        default:""
+    },
+    is_booked:{
+        type:Number,
+        default:0,
+    }
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("service", serviceSchema); 
