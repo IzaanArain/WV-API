99698d8e5ba01d13b28c68e764c6b3e2d06b4e4d
IzaanArain <izaansaquib@gmail.com>
2023-11-30 06:42:22 +0500

    updated

--- middlewares/Multer.js
+++ middlewares/Multer.js
@@ -17,20 +17,22 @@ const storage = multer.diskStorage({
     const filename = file.fieldname + "-" + unique_infix + "-" + file.originalname;
     cb(null, filename);
   },
-});
+});  
 
 const upload = multer({
   storage: storage,
-  //   fileFilter: (req, file, cb) => {
-  //     if (file.fieldname === "profile_image") {
-  //         // console.log("filefilter",req.files.profile_image)
-  //       if (!req.files.profile_image) {
-  //         cb(new Error("Image is required!"), false);
-  //       } else {
-  //         cb(null, true);
-  //       }
-  //     }
-  //   },
+    fileFilter: (req, file, cb) => {
+      console.log("files",req?.files);
+      console.log("multer file",file);
+      // if (file.fieldname === "profile_image") {
+      //     // console.log("filefilter",req.files.profile_image)
+      //   if (!req.files.profile_image) {
+      //     cb(new Error("Image is required!"), false);
+      //   } else {
+      //     cb(null, true);
+      //   }
+      // }
+    },
 });
 
 module.exports = { upload };
