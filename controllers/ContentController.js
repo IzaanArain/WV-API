e1fd7190cecba876564129232792f88e836ac6a0
IzaanArain <izaansaquib@gmail.com>
2023-11-30 07:46:20 +0500

    updated

--- controllers/ContentController.js
+++ controllers/ContentController.js
@@ -11,22 +11,22 @@ const createContent = async (req, res) => {
       "information",
     ];
     if (!title) {
-      return res.status(500).send({
+      return res.status(400).send({
         status: 0,
         message: "please enter title",
       });
     } else if (!content) {
-      return res.status(500).send({
+      return res.status(400).send({
         status: 0,
         message: "please enter content",
       });
     } else if (!type) {
-      return res.status(500).send({
+      return res.status(400).send({
         status: 0,
         message: "please enter a content type",
       });
     } else if (!types.includes(type)) {
-      return res.status(500).send({
+      return res.status(400).send({
         status: 0,
         message: `Accepted types are limited to the following: "privacy_policy", "terms_and_conditions", "about_us", "help_and_support", and "information".`,
       });
