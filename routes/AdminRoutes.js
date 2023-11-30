aeeadea1a63451512600a65b1debc8ff3ca9912c
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-11-30 20:37:26 +0500

    updated

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { upload } = require("../middlewares/Multer");
 const { signin, forgot_password, reset_password, otp_verfy,signout, complete_profile, change_password, admin_block_user, admin_delete_user } = require("../controllers/AdminController");
 const { tokenValidator } = require("../middlewares/AdminAuth");
-const { createContent } = require("../controllers/ContentController");
+const { createContent, getContent } = require("../controllers/ContentController");
+const { createService, editService, deleteService } = require("../controllers/ServicesController");
 const router = express.Router();
 
 router.post("/signin", signin);
@@ -16,5 +17,12 @@ router.post("/admin_block_user",tokenValidator, admin_block_user);
 router.post("/admin_delete_user",tokenValidator, admin_delete_user);
 /********** Content *************/
 router.post("/create_content",upload.fields([{name:"company_image",maxCount:1}]),tokenValidator, createContent);
+router.get("/get_content",tokenValidator,getContent);
+/********** Service *************/
+router.post("create_service",tokenValidator,createService);
+router.post("edit_service",tokenValidator,editService);
+router.post("delete_service",tokenValidator,deleteService);
+router.post("book_service",tokenValidator)
+
 
 module.exports = router;
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express=require("express");
 const { signin,signup, otp_verify, social_login, resend_otp, signout, complete_profile, delete_profile, book_service } = require("../controllers/UserController");
 const { tokenValidator } = require("../middlewares/Auth");
 const { upload } = require("../middlewares/Multer");
+const { getAllServices, getServiceDetails } = require("../controllers/ServicesController");
 const router=express.Router();
 
 router.post("/signin",signin);
@@ -13,6 +14,8 @@ router.post("/complete_profile",tokenValidator,upload.fields([{name:"profile_ima
 router.post("/signout",tokenValidator,signout);
 router.post("/delete_profile",tokenValidator,delete_profile);
 router.post("/book_service",tokenValidator,book_service);
-
-
+/********** Service *************/
+router.post("get_all_service",tokenValidator,getAllServices);
+router.post("get_service_details",tokenValidator,getServiceDetails);
+router.post("book_service",tokenValidator,book_service)
 module.exports=router;
